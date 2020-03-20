de302372b044ab295e49923c5434fda57ab8d21d
aonkonDey <Aonkon.dey@tufts.edu>
2020-03-20 22:09:13 +0600

    adding newly test function for new functions on Dummy Reports files

--- tests/testthat/test_results_reports.R
+++ tests/testthat/test_results_reports.R
@@ -47,3 +47,12 @@ test_that("Test that generate_results_tables creates a file when run on the User
   status = file.exists(results_output_file)
   expect_equal(status, TRUE)
 })
+
+test_that("Test that generate_results_tables creates a file when run on the Dummy Reports Survey", {
+  survey_dir = file.path(surveysdir, "Dummy Reports")
+  qsf = file.path(survey_dir, "Dummy_Survey_to_Test.qsf")
+  csv = file.path(survey_dir, "Dummy_Survey_Report.csv")
+  results_output_file = make_results_tables(headerrows=3, qsf_path = qsf, csv_path = csv)
+  status = file.exists(results_output_file)
+  expect_equal(status, TRUE)
+})
