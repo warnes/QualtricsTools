3a68fdc50abd40faa725ba6fec065da055f48754
aonkonDey <Aonkon.dey@tufts.edu>
2020-03-23 22:24:47 +0600

    changes in documentation after review from Matthew

--- R/reorganizing_survey_data.R
+++ R/reorganizing_survey_data.R
@@ -432,13 +432,19 @@ clean_question_text <- function(questions) {
 #'
 #' This function uses regex extensively to clean HTML out of a given text block.
 #' The first regex used is "\\s+". It matches multiple characters of whitespace, and
-#' reduces them to a single space character. HTML tags and Entries are then clean
+#' reduces them to a single space character. HTML tags and Entries are then cleaned.
 #' with this regular expression: "<.*?>|&[# a-z 0-9]*". It matches a substring that
 #' starts with & and ends with ; with lower case letters between them, or a substring
-#' with < and > on each side, with any characters between. The third regex expression #' mathces all text within{} except for ones that have a preceeding $- which is used #' to indicate piped text. Each matched substring is replaced with a space character.
+#' with < and > on each side, with any characters between. The third regex expression
+#' mathces all text within{} except for ones that have a preceeding $ - which is used
+#' to indicate piped text. Each matched substring is replaced with a space character.
+#' The 4th regex expression removes a specific type of CSS table formatting used by
+#' the office, starting with .Matrix and ending with .c\\d where\\d in regex represents
+#' a arbitrary digit.
 #' At the end, all extra whitespaces are removed.
 #'
-#' @param text any text string that might contain HTML or whitespace that needs to be stripped.
+#' @param text any text string that might contain HTML or whitespace that needs to be
+#' stripped.
 #' @inheritParams clean_question_text
 #' @return text without any html or extraneous whitespace.
 
