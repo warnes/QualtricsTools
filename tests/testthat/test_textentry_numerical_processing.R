cf557d99ad8dbdbb1667eac093506e526d9f26cb
Emma M <emma.morgan@tufts.edu>
2020-04-13 17:01:15 -0400

    Updating tests for text entry numeric to use here::here package and ensure that the second checks for lack of a table and presence of data that could not be coerced to numeric

--- tests/testthat/test_textentry_numerical_processing.R
+++ tests/testthat/test_textentry_numerical_processing.R
@@ -4,25 +4,20 @@ context("Testing text_entry_numerical_results")
 # Load the original_first_rows from the Dummy Enhancement Sample Survey.
 # This is needed for the next several tests that use questions from the
 # Dummy Enhancement Sample Survey.
+
 original_first_rows <- readRDS(
-  file.path(
-    find.package('QualtricsTools'),
-    'data/Sample Surveys/Dummy Enhancement Sample Survey/RDS',
-    'original_first_rows.rds'
-  )
+  here::here('data/Sample Surveys/Dummy Enhancement Sample Survey/RDS',
+    'original_first_rows.rds')
 )
 
 # Dummy Enchancement Sample Survey: Q4
 test_that(
-  "Test that text_entry_numerical_results is correct for Q4 in the Dummy Enhancement Sample Survey",
+  "Test that text_entry_numerical_results table correct for Q4 in the Dummy Enhancement Sample Survey",
   {
     # Load the question, without the results tables, for processing.
     Q4 <- readRDS(
-      file.path(
-        find.package('QualtricsTools'),
-        'data/Sample Surveys/Dummy Enhancement Sample Survey/RDS',
-        'Q4.rds'
-      )
+      here::here('data/Sample Surveys/Dummy Enhancement Sample Survey/RDS',
+        'Q4.rds')
     )
 
     # Process the question and insert results tables into it.
@@ -30,9 +25,7 @@ test_that(
 
     # Load the previously computed results table.
     Q4_results_table <-  readRDS(
-      file.path(
-        find.package('QualtricsTools'),
-        'data/Sample Surveys/Dummy Enhancement Sample Survey/RDS',
+      here::here('data/Sample Surveys/Dummy Enhancement Sample Survey/RDS',
         'Q4_results_table.rds'
       )
     )
@@ -46,13 +39,11 @@ test_that(
 
 # Dummy Enchancement Sample Survey: Q11
 test_that(
-  "Test that text_entry_numerical_results is correct for Q16 in the Dummy Enhancement Sample Survey",
+  "Test that results are not processed for numeric text entry question with text responses",
   {
     # Load the question, without the results tables, for processing.
     Q11 <- readRDS(
-      file.path(
-        find.package('QualtricsTools'),
-        'data/Sample Surveys/Dummy Enhancement Sample Survey/RDS',
+      here::here('data/Sample Surveys/Dummy Enhancement Sample Survey/RDS',
         'Q11.rds'
       )
     )
@@ -60,8 +51,12 @@ test_that(
     # Process the question and insert results tables into it.
     Q11 <- process_question_results(Q11, original_first_rows)
 
+    #Check that there are data that cannot be converted to numeric
+    Q11_responses <- as.character(Q11[["Responses"]])
+    Q11_responses <- suppressWarnings(as.numeric(Q11_responses))
+
     # No tables should be generated and if this true, the test passes.
-    expect_true(!("Table" %in% names(Q11)))
+    expect_true(!("Table" %in% names(Q11)) & any(is.na(Q11_responses)))
   }
 )
 
